4ee7025a4ef25284ac05d1d53d8ebdb26d275df5
Rubysage20 <135853594+Rubysage20@users.noreply.github.com>
2024-08-19 20:54:03 -0400

    Add files via upload

---
+++ Cirque.java
@@ -0,0 +1,41 @@
+package lab2;
+
+/*
+ * Name:Valerie Dawson
+ * Overview: This program displays Cirque du Soleil shows and cities
+ * Input: Number of tickets
+ * Output: Total
+ * Variables: N/A
+ * 
+ */
+
+public class Cirque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("////////////////////");
+		System.out.print("\\\\\\\\");
+		System.out.print("\\\\\\\\");
+		System.out.print("\\\\\\\\");
+		System.out.print("\\\\\\\\");
+		System.out.print("\\\\\\\\");
+		System.out.print("\\\\\\\\");
+		System.out.println("\n|=\t\tCirque du Soleil\t  =|");
+		System.out.print("\\\\\\\\\\\\\\");
+		System.out.print("\\\\\\\\\\\\");
+		System.out.print("\\\\\\\\\\\\\\");
+		System.out.print("//////");
+		System.out.print("//////");
+		System.out.print("//////");
+		System.out.print("//////");
+		System.out.println("\nShow\tChicago \tBaltimore\tTotal");
+		System.out.println("----\t------- \t---------\t-----");
+		System.out.println("Madive\t 36,000\t\t77,000\t\t" + (36000+77000));
+		System.out.println("Beastie\t 20,000\t\t19,000\t\t" + (20000+19000));
+		System.out.println("Sweetie\t 18,000 \t40,000\t\t" + (18000+40000));
+		System.out.println("Spirit\t 12,000 \t77,000\t\t" + (12000+77000));
+		System.out.println("Unseen\t 44,000 \t88,000\t\t" + (44000+88000));
+		
+	}
+
+}
